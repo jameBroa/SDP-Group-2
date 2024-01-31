0470184613121bc4154762fbe1ff31057100a6ba
bvechiato <beaaccounts@icloud.com>
2024-01-31 13:19:06 +0000

    Fixed #7 (API keys)

--- IntelliPutt/babel.config.js
+++ IntelliPutt/babel.config.js
@@ -2,8 +2,16 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["expo-router/babel"],
-    plugins: ['@babel/plugin-transform-unicode-property-regex'],
-    plugins: ["nativewind/babel"],
+    plugins: [
+      "nativewind/babel",
+      [
+        "module:react-native-dotenv",
+        {
+          envName: "APP_ENV",
+          moduleName: "@env",
+          path: ".env"
+        }
+      ]
+    ],
   };
 };
