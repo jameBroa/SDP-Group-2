da0dc366389f99a2cae6fa8aba73299ab010d33c
bvechiato <beaaccounts@icloud.com>
2024-03-16 17:12:25 +0000

    Shows videos like reels

--- IntelliPutt/app/home/playback.js
+++ IntelliPutt/app/home/playback.js
@@ -1,17 +1,25 @@
-import * as React from 'react';
-import { View, StyleSheet, Text, ScrollView } from 'react-native';
 import VideoPlayer from '../../components/VideoPlayer';
 import { getStorage, ref, listAll, getDownloadURL } from 'firebase/storage';
 import { useSelector } from 'react-redux';
 import { useLocalSearchParams, Stack, router } from 'expo-router';
-import BackButton from '../../components/BackButton';
+import { View, Dimensions, FlatList, StyleSheet, Pressable, Text } from 'react-native';
+import { Video, ResizeMode } from 'expo-av';
+import React, { useEffect, useRef, useState } from 'react';
 
 export default function Playback() {
     const user = useSelector((state) => state.user.user);
     const { session, date } = useLocalSearchParams();
-
     const [videos, setVideos] = React.useState([]);
 
+    const [currentViewableItemIndex, setCurrentViewableItemIndex] = useState(0);
+    const viewabilityConfig = { viewAreaCoveragePercentThreshold: 50 }
+    const onViewableItemsChanged = ({ viewableItems }) => {
+        if (viewableItems.length > 0) {
+            setCurrentViewableItemIndex(viewableItems[0].index ?? 0);
+        }
+    }
+    const viewabilityConfigCallbackPairs = useRef([{ viewabilityConfig, onViewableItemsChanged }])
+    
     const fetchVideos = async () => {
       try {
         const storage = getStorage();
@@ -29,46 +37,98 @@ export default function Playback() {
       }
     };
 
-    React.useEffect(() => {
+    useEffect(() => {
       fetchVideos(); // Fetch videos for the session when component mounts
+      setCurrentViewableItemIndex(0);
     }, []);
 
     return (
-        <View className="w-full h-full flex flex-col items-center bg-brand-colordark-green">
-            <Stack.Screen options={{headerShown:false}}></Stack.Screen>
-            <View className="h-[30%] w-full items-center justify-center mb-4">
-                <BackButton className="h-[5%]" action={() => router.back()} />
-                <View className="flex-row justify-end items-center mb-10 absolute left-[10%] top-[80%]">
-                    <View className="flex-row mb-2">
-                        <Text className="font-semibold text-3xl text-stone-50">Videos from {date}</Text>
-                    </View>
-                </View>
-            </View>     
-            <ScrollView contentContainerStyle={styles.wrapper} className="w-full flex flex-col space-y-1 rounded-2xl">
-                <View style={styles.container}>
-                    {videos.map((url, index) => (
-                        <VideoPlayer key={index} url={url} />
-                    ))}
-                    {videos.size === 0 && <Text>No videos found</Text>}
-                </View>
-            </ScrollView>
+        <View className="w-full h-full flex flex-col items-center bg-black" style={styles.videoContainer}>
+            <Stack.Screen options={{
+                headerShown: true,
+                headerStyle: {
+                    backgroundColor: '#fafaf9',
+                },
+                headerBackButtonMenuEnabled: true,
+                headerLeft: () => <Pressable onPress={() => router.replace("/home/videosPerSession") && setCurrentViewableItemIndex(0)} >
+                    <Text className="pl-5 text-base">Back</Text>
+                </Pressable>,
+                title: `Videos from ${date}`,
+                headerTintColor: '#000000',
+                headerTitleStyle: {
+                    fontSize: 16,
+                },
+                headerBackTitleVisible: true,
+            }
+            } 
+            />
+            <FlatList
+                data={videos}
+                renderItem={({ item, index }) => (
+                    <Item item={item} shouldPlay={index === currentViewableItemIndex} />
+                )}
+                keyExtractor={item => item}
+                pagingEnabled
+                horizontal={false}
+                showsVerticalScrollIndicator={false}
+                viewabilityConfigCallbackPairs={viewabilityConfigCallbackPairs.current}
+            />
         </View>
     );
 }
 
-
+const Item = ({ item, shouldPlay }) => {
+    const video = React.useRef(null);
+    const [status, setStatus] = useState(null);
+  
+    useEffect(() => {
+      if (!video.current) return;
+  
+      if (shouldPlay) {
+        video.current.playAsync()
+      } else {
+        video.current.pauseAsync()
+        video.current.setPositionAsync(0)
+      }
+    }, [shouldPlay])
+  
+    return (
+      <Pressable onPress={() => status.isPlaying ? video.current?.pauseAsync() : video.current?.playAsync()}>
+        <View style={styles.videoContainer}>
+        <Video 
+          ref={video}
+          source={{ uri: item }}
+          style={styles.video}
+          isLooping
+          resizeMode={ResizeMode.COVER}
+          useNativeControls={false}
+          onPlaybackStatusUpdate={status => setStatus(() => status)}
+        />
+      </View>
+      </Pressable>
+    );
+}
+  
 const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    videoContainer: {
+      width: Dimensions.get('window').width,
+      height: Dimensions.get('window').height - 100,
+      justifyContent: 'center',
+    },
+    video: {
+        width: "100%",
+        height: "20%",
+        alignItems: 'center',
+    },
     container: {
         flex: 1,
         justifyContent: 'center',
         backgroundColor: '#ecf0f1',
         paddingTop: 50,
         paddingBottom: 30
-    },
-    video: {
-        alignSelf: 'center',
-        width: 320,
-        height: 200,
     },
     buttons: {
         flexDirection: 'row',
