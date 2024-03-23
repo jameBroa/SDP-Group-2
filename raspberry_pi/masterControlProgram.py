725aad9e4ad8f898f57185c13d8663aaaf97fe41
bvechiato <beaaccounts@icloud.com>
2024-03-23 16:54:01 +0000

    ir sensor added to control loop

--- raspberry_pi/masterControlProgram.py
+++ raspberry_pi/masterControlProgram.py
@@ -1,6 +1,6 @@
 import serial
 import time
-
+import RPi.GPIO as GPIO
 
 code = """from PicoRobotics import KitronikPicoRobotics\n
 import utime
@@ -57,7 +57,7 @@ push(4, 200)\r
 
 class Lift():
 	def __init__(s, solenoid_speed, dc_motor_speed):
-		setup = """from PicoRobotics import KitronikPicoRobotics\r
+		s.setup = """from PicoRobotics import KitronikPicoRobotics\r
 		from mainRunner import lift_down, lift_up, push, spin, main\r
 		import utime\r
 		board=KitronikPicoRobotics()\r"""
@@ -65,9 +65,22 @@ class Lift():
 		s.solenoid_speed = 200
 		s.dc_motor = 1
 		s.dc_motor_speed = 30
+		
+		GPIO.setmode(GPIO.BCM)
+		s.overshoot_pin = 17
+		s.hole_pin = 27
+		GPIO.setup(s.overshoot_pin, GPIO.IN)
+		GPIO.setup(s.hole_pin, GPIO.IN)
+		
 		s.ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
 		s.ser.write(b'\x04')
-		s.ser.write(setup.encode())
+		s.ser.write(s.setup.encode())
+		
+	def reset_pico(s):
+		s.ser.close()
+		s.ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+		s.ser.write(b'\x04')
+		s.ser.write(s.setup.encode())
 
 	def lift_down(s):
 		liftdown = "lift_down("+str(s.dc_motor)+", "+str(s.dc_motor_speed)+")\r"
@@ -95,9 +108,17 @@ class Lift():
 	def spin(s):
 		spinn = """
 		board.servoWrite(1,0)\r
-		utime.sleep(3)\r
+		utime.sleep(0.5)\r
 		board.servoWrite(1, 80)\r"""
 		s.ser.write(spinn.encode())
+		
+	def wait_ir_trigger(s):
+		while True:
+			input_val = GPIO.input(s.overshoot_pin)
+			if input_val == 1:
+				print(input_val)
+				return
+		#time.sleep(0.005)
 	
 	# from top position
 	def up_mainloop(s):
@@ -105,17 +126,30 @@ class Lift():
 		s.lift_up()
 		s.push()
 		s.lift_down()
-	
+		time.sleep(1)
 	# from bottom position
 	def mainloop(s):
-		s.spin()
-		s.lift_up()
-		s.push()
-		s.lift_down()
-		
+		while True:
+			try:
+				s.reset_pico()
+				s.wait_ir_trigger()
+				s.spin()
+				s.lift_up()
+				s.push()
+				s.lift_down()
+				time.sleep(1)
+
+			except KeyboardInterrupt:
+				s.flush()
+				break
+				
 	def close(s):
 		s.ser.close()
+		#GPIO.cleanup()
+	
+	def flush(s):
+		flush = """"""
+		s.ser.write(b'\x04')
 
-
-lift = Lift(200,30)
+lift = Lift(solenoid_speed=200,dc_motor_speed=30)
 lift.mainloop()
